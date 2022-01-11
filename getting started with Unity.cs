774fcd4dae91de947b11b2785bc86e7a90df7dd4
Michael Royal <45159366+mikeroyal@users.noreply.github.com>
2022-01-10 18:47:33 -0800

    Getting started with Unity scripting.

---
+++ getting started with Unity.cs
@@ -0,0 +1,14 @@
+Code samples & snippets coming soon!
+
+using System;
+
+namespace Hello
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello!");    
+    }
+  }
+}
